8ca86b315022720e6a940b08a669a0c14d04f25d
Gowtham Behara <gothsiddu@gmail.com>
2018-10-18 15:58:02 +0530

    Create class to render Game

--- src/index.js
+++ src/index.js
@@ -40,3 +40,19 @@ class Board extends Component {
         );
     }
 }
+
+class Game extends Component {
+    render() {
+        return (
+            <div>
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/*status*/}</div>
+                    <ol>{/*TODO*/}</ol>
+                </div>
+            </div>            
+        );
+    }
+}
